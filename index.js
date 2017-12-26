17b1fbcfc93f548785384fb624b1fa5314c60177
Ailson Freire <ailson_freire@outlook.com>
2017-12-26 00:15:32 -0300

    Creanting a first component

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+//Import a library to help to create a component 
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
+//Create a component
+const App = () => (
+        <Text>Some Text</Text>
+);
+
+//Render it to the 
 AppRegistry.registerComponent('Albuns', () => App);
